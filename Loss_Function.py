60e90b4218eccfb6e44f21f021d09a2f9878da97
Alexius233 <74576499+Alexius233@users.noreply.github.com>
2022-04-06 15:36:59 +0800

    Add files via upload

--- Loss_Function.py
+++ Loss_Function.py
@@ -4,7 +4,7 @@ from Hyperparameters import Hyperparameters as hp
 
 
 
-def Ecos_sim(V_F, A_F, margin=0):  # vision feature, audio feature
+def Ecos_sim(V_F_single, A_F_single, margin=0):  # vision feature, audio feature
     # vision = tensor[batch_size, channels, Height, Wide] -> tensor[channels, Height, Wide]
     # audio  = tensor[batch_szie, channels, Wide]         -> tensor[channels, Wide]
 
@@ -14,91 +14,117 @@ def Ecos_sim(V_F, A_F, margin=0):  # vision feature, audio feature
     #V_F = V_F.view(sizeV[0] * sizeV[1], sizeV[2])
     #A_F = A_F.view(sizeA[0] * sizeA[0])
 
-    upper = V_F.mm(A_F.t())
+    upper = V_F_single.mm(A_F_single.t())
 
-    V_F_norm = torch.sqrt((V_F ** 2).sum(1).view(-1, 1) + 1e-18)
-    A_F_norm = torch.sqrt((A_F ** 2).sum(1) + 1e-18)
+    V_F_norm = torch.sqrt((V_F_single ** 2).sum() + 1e-18)
+    A_F_norm = torch.sqrt((A_F_single ** 2).sum(0) + 1e-18)
 
     sim = upper / (V_F_norm * A_F_norm)
 
     e_sim = torch.exp(sim - margin)
 
-    return e_sim
+    return e_sim              # 只是算一个样本的
 
 
 class ContrastiveLoss(nn.Module):
     def __init__(self,num=32):  # 假设默认num=32是batch_size
         super(ContrastiveLoss, self).__init__()
         self.num = num
+        self.cos_sim = nn.CosineSimilarity(dim=1, eps=1e-18)
 
 
     def forward(self, pre_VF, pre_AF, back_VF, back_AF):
         sim_pre = []
-        sim_back_V = []
-        sim_back_V_down = []
-        sim_back_A = []
-        sim_back_A_down = []
+        #sim_back_V = []
+        #sim_back_V_down = []
+        #sim_back_A = []
+        #sim_back_A_down = []
         # size = batch_size
-        size = self.pre_VF.size1(0)
+        size = pre_VF.size(0)
 
-        for i in range(0, size):
-            sim_pre += torch.log(Ecos_sim(pre_VF[:i, ], pre_AF[:i, ]))
 
-        for j in range(0, size):  # vision to audio
-            # now = back_VF[:j,]
+        #for i in range(0, size):
+        #    sim_pre += torch.log(Ecos_sim(pre_VF[:i, ], pre_AF[:i, ])) # [batch_size, 2048]
 
-            if self.num == 32:
-                for k in range(0, size):
+        sim_pre = self.cos_sim(pre_VF, pre_AF)   # 先行算一个普通的cos_sim
+#######################################################################
+        if self.num == hp.batch_size:
+            for i in range(0, size):  # vision to audio, 一个i算一个样本的loss
+                # now = back_VF[i]
+                if self.num == hp.batch_size:
                     for j in range(0, size):
-                        if k == j:
+                        #for j in range(0, size):
+                        if j == i:
                             continue
-                        negtive = Ecos_sim(back_VF[:k, ], back_AF[:j, ])
-                        sim_back_V_down += negtive
+                        negtive = Ecos_sim(back_VF[i], back_AF[j])
+                        if j == 0:
+                            sim_back_V_down = negtive
+                        else:
+                            sim_back_V_down += negtive
 
-                    positive = Ecos_sim(back_VF[:k, ], back_AF[:k, ], margin=hp.margin)
+                positive = Ecos_sim(back_VF[i], back_AF[i], margin=hp.margin)
 
-                    sim_back_V += torch.log(positive / positive + sim_back_V_down)
-            else:
-                for k in range(0, size):  # 从这个之后取，到末尾了还没取到对应数量，从头接着取
-                    for j in range(k, k + size):
-                        negtive = Ecos_sim(back_VF[:k, ], back_AF[:j, ])
-                        sim_back_V_down += negtive
-                        if j == size:
-                            breakpoint = j
-                            for j in range(0, size - breakpoint):
-                                sim_back_V_down += negtive
+                if i == 0:
+                    sim_back_V = torch.log(positive/ positive + sim_back_V_down)
+                else:
+                    sim_back_V = torch.cat((sim_back_V, torch.log(positive / positive + sim_back_V_down)), 0)
 
-                    positive = Ecos_sim(back_VF[:k, ], back_AF[:k, ], margin=self.margin)
+        else:
+            for i in range(0, size):  # 从这个之后取，到末尾了还没取到对应数量，从头接着取
+                for j in range(i, i + size):
+                    negtive = Ecos_sim(back_VF[i], back_AF[j])
+                    if i == 0:
+                        sim_back_V_down = negtive
 
-                    sim_back_V += torch.log(positive / positive + sim_back_V_down)
+                    if j == size:
+                        breakpoint = j
+                        for j in range(1, size - breakpoint):
+                            sim_back_V_down += negtive
 
-        for j in range(0, size):  # audio to vision
-            # now = back_VF[:j,]
+                positive = Ecos_sim(back_VF[i], back_AF[i], margin=self.margin)
 
-            if self.num == 32:
-                for k in range(0, size):
+                if i == 0:
+                    sim_back_V = torch.log(positive / positive + sim_back_V_down)
+                else:
+                    sim_back_V = torch.cat((sim_back_V, torch.log(positive / positive + sim_back_V_down)), 0)
+#######################################################################
+        if self.num == hp.batch_size:
+            for i in range(0, size):  # audio to vision
+                # now = back_AF[i]
+                if self.num == hp.batch_size:
                     for j in range(0, size):
-                        if k == j:
+                        if j == i:
                             continue
-                        negtive = Ecos_sim(back_AF[:k, ], back_VF[:j, ])
-                        sim_back_V_down += negtive
+                        negtive = Ecos_sim(back_AF[i], back_VF[j])
+                        if j == 0:
+                            sim_back_A_down = negtive
+                        else:
+                            sim_back_A_down += negtive
 
-                    positive = Ecos_sim(back_AF[:k, ], back_VF[:k, ], margin=hp.margin)
+                positive = Ecos_sim(back_AF[i], back_VF[i], margin=hp.margin)
 
-                    sim_back_A += torch.log(positive / positive + sim_back_A_down)
-            else:
-                for k in range(0, size):  # 从这个之后取，到末尾了还没取到对应数量，从头接着取
-                    for j in range(k, k + size):
-                        negtive = Ecos_sim(back_AF[:k, ], back_VF[:j, ])
-                        sim_back_A_down += negtive
-                        if j == size:
-                            breakpoint = j
-                            for j in range(0, size - breakpoint):
+                if i == 0:
+                    sim_back_A = torch.log(positive / positive + sim_back_A_down)
+                else:
+                    sim_back_A = torch.cat((sim_back_A, torch.log(positive / positive + sim_back_A_down)), 0)
+        else:
+            for i in range(0, size):  # 从这个之后取，到末尾了还没取到对应数量，从头接着取
+                for j in range(i, i + size):
+                    negtive = Ecos_sim(back_AF[i], back_VF[j])
+                    if j == 0:
+                        sim_back_A_down = negtive
+                    if j == size:
+                        breakpoint = j
+                        for j in range(1, size - breakpoint):
                                 sim_back_A_down += negtive
 
-                    positive = Ecos_sim(back_VF[:k, ], back_AF[:k, ], margin=hp.margin)
+                positive = Ecos_sim(back_VF[i], back_AF[i], margin=hp.margin)
 
-                    sim_back_A += torch.log(positive / positive + sim_back_A_down)
+                if i == 0:
+                    sim_back_A = torch.log(positive/ positive + sim_back_A_down)
+                else:
+                    sim_back_A = torch.cat((sim_back_A, torch.log(positive / positive + sim_back_V_down)), 0)
 
 
-        return hp.balance_parameter * (- 1 / hp.bias * (sim_back_V + sim_back_A)) + (1 - hp.balance_parameter)* sim_pre
+
+        return hp.balance_parameter * (- 1 / hp.bias * (sim_back_V + sim_back_A)) + (1 - hp.balance_parameter) * sim_pre
